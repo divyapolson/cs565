b2f4e4bcf843a95016a9e310b122d8c8b2e3c065
divyapolson <divyapolson@gmail.com>
2018-04-13 16:19:18 -0500

    deleted everything

--- Podfile
+++
@@ -1,5 +0,0 @@
-source 'https://github.com/CocoaPods/Specs.git'
-target 'cs565' do
-  pod 'GooglePlaces'
-  pod 'GoogleMaps'
-end
--- Pods/Target Support Files/Pods-current-place-on-map/Pods-current-place-on-map-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_current_place_on_map : NSObject
-@end
-@implementation PodsDummy_Pods_current_place_on_map
-@end
